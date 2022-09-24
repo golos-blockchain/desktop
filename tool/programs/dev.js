313ebae1bf2530e42b5035b1face2b804d8cd72c
1aerostorm <aerostorm1@gmail.com>
2022-09-24 08:15:03 +0000

    Auto-build app on node dev and node pack

--- tool/programs/dev.js
+++ tool/programs/dev.js
@@ -1,10 +1,31 @@
 const fs = require('fs')
 
+const buildBlogs = require('../controllers/buildBlogs')
+const buildMessenger = require('../controllers/buildMessenger')
+const buildWallet = require('../controllers/buildWallet')
 const buildApp = require('../controllers/buildApp')
 const execEx = require('../execEx')
+const { AppModuleTypes, detectAppModule} = require('../appModules')
 
 async function main() {
-    if (!fs.existsSync('dist/electron/index.html')) {
+    const argv = process.argv
+    let appm
+    if (argv.length !== 2) {
+        appm = detectAppModule(argv[2])
+        if (appm === AppModuleTypes.messenger) {
+            await buildMessenger()
+        } else if (appm === AppModuleTypes.wallet) {
+            await buildWallet()
+        } else if (appm === AppModuleTypes.blogs) {
+            await buildBlogs()
+        } else if (appm === AppModuleTypes.app) {
+            await buildApp()
+        } else {
+            console.error('Do not know what is ' + argv[2])
+        }
+    }
+
+    if (appm !== AppModuleTypes.app) {
         await buildApp()
     }
 
